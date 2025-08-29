65d5ea4a071622ca6f3422d4c604673e54d64adb
kenbme <guilhermekenned1@gmail.com>
2025-08-29 18:00:18 -0300

    carrinho

--- app/models/carrinho.rb
+++ app/models/carrinho.rb
@@ -2,7 +2,7 @@ class Carrinho < ApplicationRecord
   self.table_name = "carrinhos"
 
   belongs_to :usuario
-  has_many :carrinho_itens, dependent: :destroy, class_name: 'CarrinhoItem'
+  has_many :carrinho_itens, dependent: :destroy, class_name: "CarrinhoItem"
   has_many :produtos, through: :carrinho_items
 
   alias_method :itens, :carrinho_itens
