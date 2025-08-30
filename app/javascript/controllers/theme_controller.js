2b4b96e6c4d545e9dd98eb24050158791ec3ee75
kenbme <guilhermekenned1@gmail.com>
2025-08-30 15:15:46 -0300

    pega o tema do browser e seta

--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -2,17 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    const theme = localStorage.getItem("data-theme")
-    if (theme) {
-      this._set_theme(theme)
+    const is_dark = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches
+    const initial_theme = is_dark ? "dark" : "light"
+    document.documentElement.setAttribute("data-theme", initial_theme)
+    const stored_theme = localStorage.getItem("data-theme")
+    if (stored_theme) {
+      this._set_theme(stored_theme)
     }
     document.body.classList.remove("hidden")
   }
 
   change_theme() {
-    const currentTheme = document.documentElement.getAttribute("data-theme")
-    const newTheme = currentTheme === "dark" ? "light" : "dark"
-    this._set_theme(newTheme)
+    const current_theme = document.documentElement.getAttribute("data-theme")
+    const new_theme = current_theme === "dark" ? "light" : "dark"
+    this._set_theme(new_theme)
   }
 
   _set_theme(theme) {
