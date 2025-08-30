7a26652e096e11e7e9e28f0a5b3ea8a3cb188076
kenbme <guilhermekenned1@gmail.com>
2025-08-30 19:22:30 -0300

    agora front tbm tem um session

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,12 +1,14 @@
 class SessionController < ApplicationController
   def login_cliente
-    session[:usuario_id] = Usuario.find_by!(cargo: "cliente").id
-    render json: {}, status: :ok
+    usuario = Usuario.find_by!(cargo: "cliente")
+    session[:usuario_id] = usuario.id
+    render json: {session: {usuario: }}, status: :ok
   end
 
   def login_admin
-    session[:usuario_id] = Usuario.find_by!(cargo: "admin").id
-    render json: {}, status: :ok
+    usuario = Usuario.find_by!(cargo: "admin")
+    session[:usuario_id] = usuario.id
+    render json: {session: {usuario: }}, status: :ok
   end
 
   def logout
--- app/javascript/controllers/session_controller.js
+++ app/javascript/controllers/session_controller.js
@@ -1,38 +1,55 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["hello"]
+
+  connect() {
+    if (this.hasHelloTarget) {
+      const session = JSON.parse(localStorage.getItem("session"))
+      if(session) {
+        this.helloTarget.textContent = `Olá, ${session.usuario.nome}!`
+      }
+    }
+  }
+
   login_cliente(event) {
     event.preventDefault()
     const url = event.target.getAttribute("data-url")
-    send_post_request(url, "Logado como Cliente")
+    this._send_post_request(url, "Logado como Cliente")
   }
 
   login_admin(event) {
     event.preventDefault()
     const url = event.target.getAttribute("data-url")
-    send_post_request(url, "Logado como Admin")
+    this._send_post_request(url, "Logado como Admin")
   }
 
   logout(event) {
     event.preventDefault()
     const url = event.target.getAttribute("data-url")
-    send_post_request(url, "Deslogado")
+    this._send_post_request(url, "Deslogado")
   }
-}
 
-const send_post_request = (url, msg) => {
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-CSRF-Token": document.querySelector('[name="csrf-token"]').content,
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      alert(msg)
+  _send_post_request(url, msg) {
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": document.querySelector('[name="csrf-token"]').content,
+      },
     })
-    .catch((error) => {
-      console.error(error)
-    })
+      .then((response) => response.json())
+      .then((data) => {
+        if(data.session) {
+          localStorage.setItem("session", JSON.stringify(data.session))
+        } else {
+          localStorage.setItem("session", null)
+        }
+        alert(msg)
+        window.location.reload()
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
 }
