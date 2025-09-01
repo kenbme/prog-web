b0fa9a3adf7f2c903e8c8403fb153476ffccb330
kenbme <guilhermekenned1@gmail.com>
2025-09-01 16:03:26 -0300

    restrições de usuario

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,4 +2,14 @@ class Admin < ApplicationRecord
   self.table_name = "admins"
 
   belongs_to :usuario
+
+  validate :usuario_must_be_admin
+
+  private
+
+  def usuario_must_be_admin
+    unless usuario&.admin?
+      errors.add(:usuario, "deve ser do tipo admin")
+    end
+  end
 end
--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -2,4 +2,14 @@ class Cliente < ApplicationRecord
   self.table_name = "clientes"
 
   belongs_to :usuario
+
+  validate :usuario_must_be_cliente
+
+  private
+
+  def usuario_must_be_cliente
+    unless usuario&.cliente?
+      errors.add(:usuario, "deve ser do tipo cliente")
+    end
+  end
 end
