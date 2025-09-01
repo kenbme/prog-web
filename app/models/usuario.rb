30c083c8ac42705fd9cf4813a07b7e9ea3faa569
kenbme <guilhermekenned1@gmail.com>
2025-09-01 12:09:58 -0300

    format

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,7 +1,7 @@
 class Usuario < ApplicationRecord
   self.table_name = "usuarios"
 
-  enum :cargo, { cliente: 0, admin: 1 }
+  enum :cargo, {cliente: 0, admin: 1}
 
   validates :nome, presence: true
   validates :cargo, presence: true
