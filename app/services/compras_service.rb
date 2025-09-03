136b6c669874499ba137779beaf66db6473c971e
kenbme <guilhermekenned1@gmail.com>
2025-09-03 14:25:41 -0300

    refact

--- app/services/compras_service.rb
+++ app/services/compras_service.rb
@@ -1,27 +1,33 @@
 class ComprasService
   def self.create(carrinho:)
+    raise EmptyCarrinhoError if carrinho.itens.empty?
+
     ActiveRecord::Base.transaction do
       compra = Compra.create!(carrinho:, status: :pendente)
-      itens_data = carrinho.itens.map do |item|
-        {
-          compra: compra,
-          produto: item.produto,
-          quantidade: item.quantidade,
-          preco: item.produto.preco
-        }
-      end
-      raise StandardError if itens_data.empty?
-
-      CompraItem.create!(itens_data)
-
-      produtos = carrinho.itens.map do |item|
-        item.produto.estoque -= item.quantidade
-        item.produto
-      end
-      Produto.import!(produtos, on_duplicate_key_update: [:estoque])
-
+      create_compra_itens!(compra:, itens: carrinho.itens)
+      decrement_estoque!(itens: carrinho.itens)
       carrinho.itens.destroy_all
       compra
     end
+  end
+
+  def self.create_compra_itens!(compra:, itens:)
+    itens_data = itens.map do |item|
+      {
+        compra: compra,
+        produto: item.produto,
+        quantidade: item.quantidade,
+        preco: item.produto.preco
+      }
+    end
+    CompraItem.create!(itens_data)
+  end
+
+  def self.decrement_estoque!(itens:)
+    produtos = itens.map do |item|
+      item.produto.estoque -= item.quantidade
+      item.produto
+    end
+    Produto.import!(produtos, on_duplicate_key_update: [:estoque])
   end
 end
