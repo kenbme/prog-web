f55186d960dcea8d645daed493821b5b4bf3093a
kenbme <guilhermekenned1@gmail.com>
2025-09-05 16:57:51 -0300

    otimizando query de produtos

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -2,7 +2,7 @@ class ProdutosController < ApplicationController
   def index
     raise ForbiddenError unless current_user.cliente?
 
-    produtos = Produto.all
+    produtos = Produto.all.includes(:categoria, :tags).with_attached_imagens
     produtos = produtos.per_categoria(params[:categoria]) if params[:categoria].present?
     produtos = produtos.per_tags(params[:tags].split(",")) if params[:tags].present?
 
