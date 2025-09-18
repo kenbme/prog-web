3d0aa3ed225a17c71f52ff0a9e1c55f3bdf4ae1d
kenbme <guilhermekenned1@gmail.com>
2025-09-18 18:43:19 -0300

    limpando

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,23 +2,23 @@ class SessionController < ApplicationController
   def login_cliente
     usuario = Usuario.find_by!(cargo: "cliente")
     session[:usuario_id] = usuario.id
-    render json: {session: {usuario:}}, status: :ok
+    redirect_to :root
   end
 
   def login_vendedor
     usuario = Usuario.find_by!(cargo: "vendedor")
     session[:usuario_id] = usuario.id
-    render json: {session: {usuario:}}, status: :ok
+    redirect_to :root
   end
 
   def login_admin
     usuario = Usuario.find_by!(cargo: "admin")
     session[:usuario_id] = usuario.id
-    render json: {session: {usuario:}}, status: :ok
+    redirect_to :root
   end
 
   def logout
     session[:usuario_id] = nil
-    render json: {}, status: :ok
+    redirect_to :login
   end
 end
