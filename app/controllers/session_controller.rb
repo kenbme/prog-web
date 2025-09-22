f231974244cc7d17226eb327853ed897363df1ee
kenbme <guilhermekenned1@gmail.com>
2025-09-21 22:36:29 -0300

    refact

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,23 +2,23 @@ class SessionController < ApplicationController
   def login_cliente
     usuario = Usuario.find_by!(cargo: "cliente")
     session[:usuario_id] = usuario.id
-    redirect_to root_path
+    render json: {}
   end
 
   def login_vendedor
     usuario = Usuario.find_by!(cargo: "vendedor")
     session[:usuario_id] = usuario.id
-    redirect_to root_path
+    render json: {}
   end
 
   def login_admin
     usuario = Usuario.find_by!(cargo: "admin")
     session[:usuario_id] = usuario.id
-    redirect_to root_path
+    render json: {}
   end
 
   def logout
     session[:usuario_id] = nil
-    redirect_to root_path
+    render json: {}
   end
 end
--- app/javascript/controllers/simple_controller.js
+++ app/javascript/controllers/simple_controller.js
@@ -1,51 +1,45 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = { url: String, reload: Boolean }
+  static values = { url: String, reload: Boolean, redirect: String }
 
   create() {
-    fetch(this.urlValue, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-CSRF-Token": document.querySelector('[name="csrf-token"]').content,
-      },
-    })
-      .then((res) => {
-        if (!res.ok) throw res
-        return res.json()
-      })
-      .then((data) => {
-        alert("Sucesso!")
-        if (this.reloadValue) {
-          window.location.reload()
-        }
-      })
-      .catch((error) => {
-        console.error(error)
-      })
+    this._sendRequest("POST")
   }
 
   delete() {
+    this._sendRequest("DELETE")
+  }
+
+  _sendRequest(method) {
     fetch(this.urlValue, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        "X-CSRF-Token": document.querySelector('[name="csrf-token"]').content,
-      },
+      method: method,
+      headers: this._headers(),
     })
       .then((res) => {
         if (!res.ok) throw res
         return res.json()
       })
-      .then((data) => {
-        alert("Sucesso!")
-        if (this.reloadValue) {
-          window.location.reload()
-        }
-      })
+      .then(() => this._handleResponse())
       .catch((error) => {
-        console.error(error)
+        console.error("Erro na requisição:", error)
       })
+  }
+
+  _handleResponse() {
+    alert("Sucesso!")
+
+    if (this.reloadValue) {
+      window.location.reload()
+    } else if (this.redirectValue) {
+      window.location.href = this.redirectValue
+    }
+  }
+
+  _headers() {
+    return {
+      "Content-Type": "application/json",
+      "X-CSRF-Token": document.querySelector('[name="csrf-token"]').content,
+    }
   }
 }
